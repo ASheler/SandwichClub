40d9c1a0fb89d09f2211f7df5766314d2f82bfec
Sheler <ondraglaser@gmail.com>
2018-02-18 11:37:03 +0100

    edit code as per Mentor notes!

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -12,27 +12,38 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String JSON_NAME_KEY = "name";
+    public static final String JSON_MAINNAME_KEY = "mainName";
+    public static final String JSON_ALSOKNOWNAS_KEY = "alsoKnownAs";
+    public static final String JSON_PLACEOFORIGIN_KEY = "placeOfOrigin";
+    public static final String JSON_DESCRIPTION_KEY = "description";
+    public static final String JSON_IMAGE_KEY = "image";
+    public static final String JSON_INGREDIENTS_KEY = "ingredients";
+
+
     public static Sandwich parseSandwichJson(String json) throws JSONException {
+
+
 
         //decode json here
         JSONObject jsonRoot = new JSONObject(json);
-        JSONObject name = jsonRoot.getJSONObject("name");
-        String mainName = name.getString("mainName");
+        JSONObject name = jsonRoot.optJSONObject(JSON_NAME_KEY);
+        String mainName = name.optString(JSON_MAINNAME_KEY);
         //JSON Array for alsoKnownAs Array
-        JSONArray alsoKnownAsJSONArray = name.getJSONArray("alsoKnownAs");
+        JSONArray alsoKnownAsJSONArray = name.optJSONArray(JSON_ALSOKNOWNAS_KEY);
         List<String> alsoKnownAs = new ArrayList<>();
         for (int i = 0; i < alsoKnownAsJSONArray.length(); i++) {
-            String val = alsoKnownAsJSONArray.getString(i);
+            String val = alsoKnownAsJSONArray.optString(i);
             alsoKnownAs.add(val);
         }
-        String placeOfOrigin = jsonRoot.getString("placeOfOrigin");
-        String description = jsonRoot.getString("description");
-        String image = jsonRoot.getString("image");
+        String placeOfOrigin = jsonRoot.optString(JSON_PLACEOFORIGIN_KEY);
+        String description = jsonRoot.optString(JSON_DESCRIPTION_KEY);
+        String image = jsonRoot.optString(JSON_IMAGE_KEY);
         //JSON Array for Ingredients
-        JSONArray ingredientsJSONArray = jsonRoot.getJSONArray("ingredients");
+        JSONArray ingredientsJSONArray = jsonRoot.optJSONArray(JSON_INGREDIENTS_KEY);
         List<String> ingredients = new ArrayList<>();
         for (int i = 0; i < ingredientsJSONArray.length(); i++) {
-            String val = ingredientsJSONArray.getString(i);
+            String val = ingredientsJSONArray.optString(i);
             ingredients.add(val);
         }
 
